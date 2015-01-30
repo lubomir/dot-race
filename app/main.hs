b5ee091d77ee99ce3fb7df3ae4959723e2823356
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2015-01-30 21:39:04 +0100

    Drop printing current directory

--- app/main.hs
+++ app/main.hs
@@ -1,11 +1,9 @@
 import Prelude     (IO)
 import Application (appMain)
-import System.Directory
 import System.Environment
 
 main :: IO ()
 main = do
     print "starting"
-    getCurrentDirectory >>= print
     getArgs >>= print
     appMain
