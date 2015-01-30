1413af0dab2888c1459163b475f62ddd39e68928
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2015-01-30 21:45:52 +0100

    Don't load settings from arguments

--- app/main.hs
+++ app/main.hs
@@ -1,9 +1,7 @@
-import Prelude     (IO, (>>=), print)
+import Prelude     (IO)
 import Application (appMain)
 import System.Environment
 
 main :: IO ()
 main = do
-    print "starting"
-    getArgs >>= print
     appMain
