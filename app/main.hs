4803dc0faf695ebad133208dd0bbb4415886f396
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2015-01-30 21:36:37 +0100

    Print arguments

--- app/main.hs
+++ app/main.hs
@@ -1,9 +1,11 @@
 import Prelude     (IO)
 import Application (appMain)
 import System.Directory
+import System.Environment
 
 main :: IO ()
 main = do
     print "starting"
     getCurrentDirectory >>= print
+    getArgs >>= print
     appMain
